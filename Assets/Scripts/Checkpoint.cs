6b2f40a390ab92e37204e284fa19a2bfda6790e7
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-03-30 18:56:13 -0300

    Added Checkpoint details
    Added sound effect & material change when active

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,9 +5,21 @@ public class Checkpoint : MonoBehaviour
     private PlayerScript player;
     public Transform spawnDestination;
 
+    public Renderer checkpoint;
+    public Material inactiveVisual;
+    public Material activeVisual;
+
+    public AudioSource audioSource;
+    public AudioClip activateSound;
+
     void Start()
     {
         player = FindObjectOfType<PlayerScript>();
+
+        if (checkpoint != null && inactiveVisual != null)
+        {
+            checkpoint.material = inactiveVisual;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,6 +27,12 @@ public class Checkpoint : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             player.SetCheckpoint(spawnDestination);
+
+            if (checkpoint != null && activeVisual != null)
+            {
+                checkpoint.material = activeVisual;
+                audioSource.PlayOneShot(activateSound);
+            }
         }
     }
 }
