5b5e9abc1bb03bc5ce33c2e0fa7b1e650ff23591
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-04-05 22:56:25 -0300

    Added level select

--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -3,9 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject LevelSelect;
+
+    public void LoadLevelSelect()
+    {
+        LevelSelect.SetActive(true);
+    }
+
+    public void UnLoadLevelSelect()
+    {
+        LevelSelect.SetActive(false);
+    }
+
     public void LoadTutorial()
     {
         SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
     }
 
     public void QuitGame()
